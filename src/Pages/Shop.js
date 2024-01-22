b915249cb821b5033cc0398c34741530c6c21289
armohsin <89929080+armohsin@users.noreply.github.com>
2024-01-22 14:37:18 +0500

    Update Shop.js

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -60,14 +60,7 @@ const Shop = () => {
             discountedPrice: "$149",
             actualPrice: "$199",
         },
-        {
-            id: 5,
-            imageUrl: "https://images.unsplash.com/photo-1568819317551-31051b37f69f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            brand: "BeachWear",
-            name: "Summer Suit",
-            discountedPrice: "$149",
-            actualPrice: "$199",
-        },
+       
         // Add more items as needed
     ];
 
